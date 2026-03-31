b792c4ab4c5d5da97ceff9ce15664c196e97adcd
Logan Adams <logan.fox.adams421@gmail.com>
2026-03-31 14:52:53 -0400

    Minor work with animator testor

--- Assets/Scripts/AnimtorTestor.cs
+++ Assets/Scripts/AnimtorTestor.cs
@@ -5,6 +5,8 @@ public class AnimtorTestor : MonoBehaviour
     Animator animator;
     CharacterController controller;
 
+    private float time = 0;
+
     private void Start()
     {
         TryGetComponent<Animator>(out animator);
@@ -13,6 +15,31 @@ public class AnimtorTestor : MonoBehaviour
 
     private void Update()
     {
-        animator.SetBool("run", true);
+        /*if(time < 3f)
+            animator.SetBool("run", true);
+
+        if (time > 3f)
+            animator.SetBool("run", false);*/
+
+
+
+        /*animator.SetBool("lightattack", true);
+
+        if(time > .25f) 
+            animator.SetBool("lightattack", false);*/
+
+        /*animator.SetBool("jump", true);
+        if (time > .25f)
+            animator.SetBool("jump", false);*/
+
+        /*animator.SetBool("sprin", true);
+        if (time > .25f)
+            animator.SetBool("sprin", false);*/
+
+        animator.SetBool("heavyattack", true);
+        if (time > 0.25f)
+            animator.SetBool("heavyattack", false);
+
+        time += Time.deltaTime;
     }
 }
