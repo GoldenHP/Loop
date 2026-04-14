7d93d7fc7b6cf1f0d7aa7acb763721a129dadf7f
Logan Adams <logan.fox.adams421@gmail.com>
2026-04-14 16:45:38 -0400

    RAH (Skeleton doing things, Working on Player Camera)

--- Assets/Scripts/ControlCamera.cs
+++ Assets/Scripts/ControlCamera.cs
@@ -3,6 +3,14 @@ using UnityEngine.InputSystem;
 
 public class ControlCamera : MonoBehaviour
 {
+    [Header("Target")]
+    public Transform Player;
+    public Vector3 pivotOffset = new Vector3 (0f, 1.6f, 0f);
+
+    [Header("Orbit")]
+    public float followSpeed = 10f;
+    public float returnDelay = 1.2f;
+
     [Header("Sensitivity")]
     public float mouseSense = 0.15f;
     public float joyStickSense = 180f;
@@ -11,19 +19,45 @@ public class ControlCamera : MonoBehaviour
     public float minPinch = -80f;
     public float maxPinch = 80f;
 
+    [Header("Distance")]
+    public float DesiredDistance = 4f;
+    public float minDistance = 0.5f;
+
     [Header("Invert Y")]
     public bool invertY = false;
 
     [Header("Camera")]
+    public GameObject Camera;
 
+    [Header("Collision")]
+    public float collisionRadius = 0.2f;
+    public LayerMask collisonMask;
 
-    private float _yaw;
-    private float _pitch;
+    private float yaw;
+    private float pitch;
+    private float currentDistance;
+    private float targetYaw;
 
-    private InputAction _lookAction;
-    private bool isGamepad;
 
     private Vector2 cameraMovement;
+
+    private void Start()
+    {
+        Camera = GameObject.FindGameObjectWithTag("PlayerCamera");
+        if(Camera == null)
+        {
+            Debug.LogError("Couldnt Find Main Cmaera");
+        }
+
+        yaw = Player.eulerAngles.y;
+        targetYaw = yaw;
+        pitch = 15f;
+        currentDistance = DesiredDistance;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
 
     public void CameraMouseMove(InputAction.CallbackContext context)
     {
@@ -37,10 +71,28 @@ public class ControlCamera : MonoBehaviour
         if(invertY)
             deltaY = -deltaY;
 
-        _yaw += deltaX;
-        _pitch += deltaY;
-        _pitch = Mathf.Clamp(_pitch, minPinch, maxPinch);
+        yaw += deltaX;
+        pitch += deltaY;
+        pitch = Mathf.Clamp(pitch, minPinch, maxPinch);
 
-        transform.rotation = Quaternion.Euler(_yaw, _pitch, 0f);
+        Camera.transform.rotation = Quaternion.Euler(yaw, pitch, 0f);
+    }
+
+    public void CameraControllerMove(InputAction.CallbackContext context) 
+    {
+        cameraMovement = context.ReadValue<Vector2>();
+
+        float deltaX, deltaY;
+        deltaX = cameraMovement.x * joyStickSense;
+        deltaY = cameraMovement.y * joyStickSense;
+
+        if (invertY)
+            deltaY = -deltaY;
+
+        yaw += deltaX;
+        pitch += deltaY;
+        pitch = Mathf.Clamp(pitch, minPinch, maxPinch);
+
+        Camera.transform.rotation = Quaternion.Euler(yaw, pitch, 0f);
     }
 }
--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -8,12 +8,24 @@ public class EnemyScript : MonoBehaviour
     [SerializeField] public float enemyMaxSpeed = 10f;
     [SerializeField] public int enemyAttackDamage = 2;
 
-    [Header("RayCast Box")]
-    [SerializeField] public GameObject RaycastBox;
+    [Header("Debug Colors")]
+    public Color HitColor = Color.red;
+    public Color MissColor = Color.green;
+
+    [Header("Looking Around Settings")]
+    public float LookSpeed = 30f;
+    public float MinAngle = -60f;
+    public float MaxAngle = 60f;
+
+    [Header("Detection")]
+    public LayerMask PlayerLayer;
 
     private Animator animator;
+    private GameObject TrackingTarget;
 
-    private Ray ray;
+    private float currentAngle = 0f;
+    public float targetAngle = 0f;
+    private Quaternion startRotation;
 
     private void Start()
     {
@@ -21,25 +33,76 @@ public class EnemyScript : MonoBehaviour
         {
             Debug.LogError("Skeleton Animator Not Found");
         }
+
+        startRotation = transform.rotation;
+        targetAngle = MaxAngle;
     }
 
     public void Update()
     {
-        ray = new Ray(RaycastBox.transform.position, RaycastBox.transform.forward);
+        Ray ray = new Ray(transform.position + Vector3.up, transform.forward);
         RaycastHit hit;
-        OnDrawGizmos();
 
-        if (Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out hit, 100f)) 
         {
-            Debug.Log(hit.collider.name);
+            Debug.DrawRay(transform.position + Vector3.up, transform.forward * hit.distance, HitColor);
+            Debug.DrawLine(hit.point, hit.point + Vector3.up * 0.5f, HitColor);
 
             if(hit.collider.gameObject.CompareTag("Player"))
             {
-                Vector3 MovementVector = Vector3.Lerp(transform.position, hit.collider.transform.position, Time.deltaTime);
-                animator.SetBool("walk", true);
-                transform.Translate(MovementVector);
+
+                if(hit.collider.gameObject.CompareTag("Player"))
+                {
+                    TrackingTarget = hit.collider.gameObject;
+                }
+                else
+                {
+                    TrackingTarget = null;
+                }
             }
         }
+
+        EnemyMove(hit);
+    }
+
+    public void EnemyMove(RaycastHit hit)
+    {
+        if(TrackingTarget != null)
+        {
+            if (hit.distance < 5f)
+            {
+                //Attack()
+            }
+            else if (hit.distance < 30f)
+            {
+                animator.SetBool("walk", true);
+                animator.SetBool("run", false);
+
+                Vector3 EnemyMovementVector = Vector3.Lerp(transform.position, TrackingTarget.transform.position, Time.deltaTime / 10f);
+                transform.position = EnemyMovementVector;
+            }
+            else
+            {
+                animator.SetBool("run", true);
+                animator.SetBool("walk", false);
+
+                Vector3 EnemyMovementVector = Vector3.Lerp(transform.position, TrackingTarget.transform.position, Time.deltaTime);
+                transform.position = EnemyMovementVector;
+            }
+        }
+        if(TrackingTarget == null)
+        {
+            LookAround();
+        }
+    }
+
+    public void LookAround()
+    {
+        currentAngle = Mathf.MoveTowards(currentAngle, targetAngle, LookSpeed * Time.deltaTime);
+        transform.rotation = startRotation * Quaternion.Euler(0f, currentAngle, 0f);
+
+        if(Mathf.Approximately(currentAngle, targetAngle))
+            targetAngle = (targetAngle == MaxAngle) ?  MinAngle : MaxAngle;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -53,10 +116,5 @@ public class EnemyScript : MonoBehaviour
         {
 
         }
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawRay(ray);
     }
 }
